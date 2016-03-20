626fcbcdd9be3801342d63c09553d81aeb5cc378
khangharoth <kuldeepsinghk@gmail.com>
2016-03-20 16:16:26 +0530

    commit

--- sicpExamples/src/L4_A/newPatternMatcher.clj
+++ sicpExamples/src/L4_A/newPatternMatcher.clj
@@ -5,6 +5,9 @@
 (defn atomic? [exp]
   (not (list? exp)))
 
+(defn isEmpty?[exp]
+  (and (list? exp) (empty? exp)))
+
 (defn cadr [list]
   (first (rest list)))
 
@@ -31,15 +34,14 @@
 (defn variable-name [pattern] (cadr pattern))
 
 (defn assq [var dict]
-  (cond (= make-empty-dictionary dict) 'failed
+  (cond (= make-empty-dictionary dict) nil
     (= (first (first dict)) var) (first dict)
     :else (assq var (rest dict))))
 
 (defn extend-dictionary [pat dat dictionary]
   (let [vname (variable-name pat)]
     (let [v (assq vname dictionary)]
-      (cond (not v)
-        (cons (list vname dat) dictionary)
+      (cond (nil? v) (cons (list vname dat) dictionary)
         (= (cadr v) dat) dictionary
         :else 'failed
 
@@ -47,13 +49,20 @@
 
 (defn lookup [var dictionary]
   (let [v (assq var dictionary)]
-    (if (not v)
-      var
+    (if (nil? v) var
       (cadr v))))
+
+(defn evaluate [form dictionary]
+  (if (atomic? form)
+    (lookup form dictionary)
+    (apply (eval (lookup (first form) dictionary))
+      (map (fn [v] (lookup v dictionary))
+        (rest form)))))
 
 
 (defn match [pattern expression dictionary]
   (cond (and (nil? pattern) (nil? expression)) dictionary
+    (and (isEmpty? pattern) (isEmpty? expression)) dictionary
     (= dictionary 'failed) 'failed
 
     (atomic? pattern) (if
@@ -78,18 +87,13 @@
               (first expression)
               dictionary))))
 
+
 (defn skeleton-evaluation? [skeleton]
   (if (list? skeleton) (= (first skeleton) '$) false))
 
 (defn evaluation-expression [evaluation] (cadr evaluation))
 
-(defn evaluate [form dictionary]
-  (if (atomic? form)
-    (lookup form dictionary)
-    (apply (eval (lookup (first form) dictionary)
-             user-initial-environment)
-      (map (fn [v] (lookup v dictionary))
-        (rest form)))))
+
 
 (defn instantiate [skeleton dictionary]
   (cond (nil? skeleton) '()
@@ -106,8 +110,7 @@
 
 ;(def pat-1 '(+ (* (?x) (?y)) (?y)))
 ;(def exp-1 '(+ (* 3 x) x))
-
-(def pat-1 '(+ (?x) (?y)))
+(def pat-1 '(+ (? x) (? y)))
 (def exp-1 '(+ 3 x))
 
 (println (match pat-1 exp-1 make-empty-dictionary))
